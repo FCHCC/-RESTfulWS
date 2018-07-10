a396aac82ce5696ce01462000c182d658edf5182
FCHCC <fccachon@hotmail.com>
2018-07-10 16:10:24 -0500

    modifique metodo GetNofiticationsJSON

--- CS13304-1.12.2/src/main/java/me/jmll/utm/rest/NotificationRest.java
+++ CS13304-1.12.2/src/main/java/me/jmll/utm/rest/NotificationRest.java
@@ -1,6 +1,8 @@
 package me.jmll.utm.rest;
 
+import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.http.HttpHeaders;
@@ -10,14 +12,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import me.jmll.utm.model.Link;
 import me.jmll.utm.model.OptionsDoc;
-import me.jmll.utm.service.FileService;
+import me.jmll.utm.service.NotificationService;
 
 @Controller
 public class NotificationRest {
 
-	FileService fileService;
+	NotificationService notifications;
 	
 	@RequestMapping(value = "notify", method = RequestMethod.OPTIONS)
 	public ResponseEntity<?> showOptions(){
@@ -34,6 +40,31 @@ public class NotificationRest {
 		
 		return new ResponseEntity<>(options,headers,HttpStatus.OK);
 	}
+	
+	@RequestMapping(value="notify",
+					method= RequestMethod.GET,
+					produces = {"application/json", "text/json"})
+	@ResponseBody
+	@ResponseStatus(HttpStatus.OK)
+	public Map<String,Object> GetNotificationsJSON() {
+		
+		List<Link> _links = new ArrayList<Link>();
+		
+		_links.add(new Link(ServletUriComponentsBuilder
+		 .fromCurrentServletMapping().path("/")
+		 .build().toString(), "api"));
+		
+		_links.add(new Link(ServletUriComponentsBuilder
+		 .fromCurrentServletMapping().path("/notify/")
+		 .build().toString(), "self"));
+		
+		Map<String, Object> response = new Hashtable<>(2);
+		response.put("_links", _links);
+		response.put("data", notifications);
+		return response;
+
+	}
+	
 	
 	
 	
