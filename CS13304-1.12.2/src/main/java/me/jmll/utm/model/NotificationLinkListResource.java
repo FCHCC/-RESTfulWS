49ce41eafecfd217996e582cb31095d453ad7fe0
FCHCC <fccachon@hotmail.com>
2018-07-10 16:26:27 -0500

    cree NotificationLinkListResource en paquete model

---
+++ CS13304-1.12.2/src/main/java/me/jmll/utm/model/NotificationLinkListResource.java
@@ -0,0 +1,37 @@
+package me.jmll.utm.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class NotificationLinkListResource {
+	
+	private List<Link> links = new ArrayList<>();
+	private List<Notification> notifications = new ArrayList<>();
+	
+	@XmlElement(name="link")
+	public List<Link> getLinks() {
+		return links;
+	}
+	public void setLinks(List<Link> links) {
+		this.links = links;
+	}
+	
+	public void addLink(Link link) {
+		this.links.add(link);
+	}
+	
+	@XmlElement(name="data")
+	public List<Notification> getNotifications() {
+		return notifications;
+	}
+	public void setNotifications(List<Notification> notifications) {
+		this.notifications = notifications;
+	}
+	
+	
+	
+}
