c2d6065fc5ddd427a68c588fd5543cb5e1dfabd6
FCHCC <fccachon@hotmail.com>
2018-07-10 12:05:57 -0500

    añadi constructores, getter y setters

--- CS13304-1.12.2/src/main/java/me/jmll/utm/model/Notification.java
+++ CS13304-1.12.2/src/main/java/me/jmll/utm/model/Notification.java
@@ -1,5 +1,96 @@
 package me.jmll.utm.model;
 
-public class Notification {
+import java.io.Serializable;
+import java.util.List;
+import java.util.UUID;
 
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Notification implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	private String subject, message, messageId, status;
+	private List<String> toAddress,ccAddress;
+
+	public Notification(){
+		this.messageId = UUID.randomUUID().toString();
+	}
+	
+	public Notification(String message,String subject,List<String>toAddress, List<String>ccAddress) {
+	
+		this.message = message;
+		this.messageId = UUID.randomUUID().toString();
+		this.subject = subject;
+		this.toAddress = toAddress;
+		this.ccAddress = ccAddress;
+	}
+	
+	public Notification(String message, String subject, List<String>toAddress) {
+		this.message= message;
+		this.subject = subject;
+		this.toAddress = toAddress;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getMessageId() {
+		return messageId;
+	}
+
+	public void setMessageId(String messageId) {
+		this.messageId = messageId;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public List<String> getToAddress() {
+		return toAddress;
+	}
+
+	public void setToAddress(List<String> toAddress) {
+		this.toAddress = toAddress;
+	}
+	
+	public List<String> getCcAddress() {
+		return ccAddress;
+	}
+
+	public void setCcAddress(List<String> ccAddress) {
+		this.ccAddress = ccAddress;
+	}
+	
+	public String toString() {
+
+		return String.format(
+				"[id: %s: %s] to: %s; cc: %s; subject: %s; message: %s",
+				this.getMessageId(), 
+				this.getStatus(), 
+				this.getToAddress(), 
+				this.getCcAddress(), 
+				this.getSubject(), 
+				this.getMessage());
+
+	}
+	
 }
